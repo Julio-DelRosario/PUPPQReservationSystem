2fba44c4cac9bb7d2af83c16d9bfc1705aaa0378
Julio-DelRosario <150268092+Julio-DelRosario@users.noreply.github.com>
2024-06-25 15:16:48 +0800

    Testing commit

--- src/reservationsystem/UserLogin.java
+++ src/reservationsystem/UserLogin.java
@@ -2,10 +2,5 @@ package reservationsystem;
 
 
 public class UserLogin {
-    
-    public static void main (String[] args){
-        System.out.println("Test");
-        System.out.println("HEllo");
-    }
-    
+
 }
